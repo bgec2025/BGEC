432c6b0c566ebeb8c911f44d3787acdd4e12b5f7
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2025-06-12 22:50:13 +0530

    main files added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/pages/home/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Home Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    login: {
+      entry: 'src/pages/login/main.js',
+      template: 'public/login.html',
+      filename: 'login.html',
+      title: 'Login Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'login']
+    },
+    event: {
+      entry: 'src/pages/event/main.js',
+      template: 'public/event.html',
+      filename: 'event.html',
+      title: 'Event Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'event']
+    },
+    leaderboard: {
+      entry: 'src/pages/leaderboard/main.js',
+      template: 'public/leaderboard.html',
+      filename: 'leaderboard.html',
+      title: 'Leaderboard Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'leaderboard']
+    }
+  }
+};
