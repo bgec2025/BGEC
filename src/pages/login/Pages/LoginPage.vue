0b79d18aababb9e2f2c350a5cb537b7d62815945
Sheil <maniarsheil@gmail.com>
2025-06-25 01:17:41 +0530

    code cleanup

--- src/pages/login/Pages/LoginPage.vue
+++ src/pages/login/Pages/LoginPage.vue
@@ -27,7 +27,6 @@
       <!-- Black Box Login Section -->
       <div class="login-section" ref="loginBoxRef">
         <div class="login-content">
-          <!-- Gaming SVG Elements -->
 
           <!-- Welcome Text -->
           <div class="welcome-text" ref="welcomeTextRef">
@@ -44,12 +43,6 @@
               :disabled="isLoading"
               ref="googleBtnRef"
             >
-              <svg class="google-icon" viewBox="0 0 24 24">
-                <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
-                <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
-                <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
-                <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
-              </svg>
               <span>{{ isLoading ? 'Signing in...' : 'Sign in with Google' }}</span>
             </button>
           </div>
@@ -172,14 +165,6 @@ const initAnimations = () => {
     ease: 'power3.out'
   }, '-=0.8')
   
-  // Gaming icons float animation
-  .from('.gaming-svg', {
-    opacity: 0,
-    y: 30,
-    duration: 0.6,
-    stagger: 0.2,
-    ease: 'power2.out'
-  }, '-=0.6')
   
   // Welcome text stagger
   .from('.main-heading', {
@@ -216,28 +201,6 @@ const initAnimations = () => {
 }
 
 const initFloatingAnimations = () => {
-  // Gaming icons floating animation
-  // eslint-disable-next-line no-undef
-  const gamingIcons = document.querySelectorAll('.gaming-svg')
-  gamingIcons.forEach((icon, index) => {
-    gsap.to(icon, {
-      y: -15 + (index * 5),
-      duration: 2 + (index * 0.5),
-      ease: 'sine.inOut',
-      repeat: -1,
-      yoyo: true,
-      delay: index * 0.3
-    })
-    
-    gsap.to(icon, {
-      rotation: 5 - (index * 2),
-      duration: 3 + index,
-      ease: 'sine.inOut',
-      repeat: -1,
-      yoyo: true,
-      delay: index * 0.2
-    })
-  })
   
   // Logo subtle animation
   gsap.to('.logo-image', {
@@ -276,25 +239,6 @@ const initHoverAnimations = () => {
     })
   }
   
-  // Gaming icons hover
-  // eslint-disable-next-line no-undef
-  document.querySelectorAll('.gaming-svg').forEach(icon => {
-    icon.addEventListener('mouseenter', () => {
-      gsap.to(icon, {
-        scale: 1.2,
-        duration: 0.3,
-        ease: 'back.out(1.7)'
-      })
-    })
-    
-    icon.addEventListener('mouseleave', () => {
-      gsap.to(icon, {
-        scale: 1,
-        duration: 0.3,
-        ease: 'power2.out'
-      })
-    })
-  })
 }
 
 // Lifecycle
